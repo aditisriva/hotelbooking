e346d89a02373be69e6e0d9e29981ba1a5f0842c
Aditi Srivastava <aditi.s@humppl.com>
2026-07-13 12:11:56 +0530

    fix: correct bind_param type string in bhSeedHotels (15 params, was 16)

--- hotel/hotel_functions.php
+++ hotel/hotel_functions.php
@@ -30,7 +30,7 @@ function bhSeedHotels(): void {
          VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
     );
     foreach ($seeds as $s) {
-        mysqli_stmt_bind_param($stmt, 'sssssddddisssiis',
+        mysqli_stmt_bind_param($stmt, 'sssssddddissiis',
             $s[0],$s[1],$s[2],$s[3],$s[4],$s[5],$s[6],$s[7],$s[8],$s[9],$s[10],$s[11],$s[12],$s[13],$s[14]
         );
         mysqli_stmt_execute($stmt);
